bb732618eca5ac4eac1dec304413fd4c08530c86
Sahithi5555 <snsahithi05@gmail.com>
2026-03-25 14:11:07 +0530

    UC1: Application Entry & Welcome Message

---
+++ app/src/BookMyStayApp.java
@@ -0,0 +1,8 @@
+public class BookMyStayApp {
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to BookMyStay!");
+        System.out.println("Hotel Booking System v1.0");
+
+    }
+}
